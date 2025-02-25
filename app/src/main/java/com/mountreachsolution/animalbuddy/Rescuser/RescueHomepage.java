f96fc497489279b5eca453d62aefaf8c27241db6
Parth200611 <parthdahapute7@gmail.com>
2025-02-25 13:57:24 +0530

    commit

--- app/src/main/java/com/mountreachsolution/animalbuddy/Rescuser/RescueHomepage.java
+++ app/src/main/java/com/mountreachsolution/animalbuddy/Rescuser/RescueHomepage.java
@@ -1,22 +1,65 @@
 package com.mountreachsolution.animalbuddy.Rescuser;
 
+import android.graphics.drawable.ColorDrawable;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.Window;
+import android.view.WindowInsetsController;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
+import androidx.core.content.ContextCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.mountreachsolution.animalbuddy.R;
+import com.mountreachsolution.animalbuddy.User.UserHomepage;
 
-public class RescueHomepage extends AppCompatActivity {
+public class RescueHomepage extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
+    BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_rescue_homepage);
+        getSupportActionBar().setBackgroundDrawable(new ColorDrawable(getResources().getColor(R.color.green)));
+        Window window = getWindow();
+        window.setStatusBarColor(getResources().getColor(R.color.green));
 
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.setDecorFitsSystemWindows(false);
+            window.getInsetsController().setSystemBarsAppearance(0, WindowInsetsController.APPEARANCE_LIGHT_STATUS_BARS);
+        }
+
+        getWindow().setNavigationBarColor(ContextCompat.getColor(RescueHomepage.this,R.color.white));
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+
+        bottomNavigationView = findViewById(R.id.bottomnevigatiomuserhome);
+        bottomNavigationView.setOnNavigationItemSelectedListener(this);
+
+        bottomNavigationView.setSelectedItemId(R.id.RViewComplain);
+
+    }
+    ViewComplain viewComplain=new ViewComplain();
+    ViewNgo viewNgo = new ViewNgo();
+    RescuProfil rescuProfil = new RescuProfil();
+
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.RViewComplain){
+            getSupportFragmentManager().beginTransaction().replace(R.id.FrameLayoutuserhome,viewComplain).commit();
+        }else if(item.getItemId()==R.id.RNgo){
+            getSupportFragmentManager().beginTransaction().replace(R.id.FrameLayoutuserhome,viewNgo).commit();
+        } else if(item.getItemId()==R.id.RProfil){
+            getSupportFragmentManager().beginTransaction().replace(R.id.FrameLayoutuserhome,rescuProfil).commit();
+        }
+        return true;
     }
 }
