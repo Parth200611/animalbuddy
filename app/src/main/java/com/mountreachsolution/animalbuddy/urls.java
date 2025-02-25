1f4fefbab3ff25cd3fc86c75fc9f086e703a52f8
Parth200611 <parthdahapute7@gmail.com>
2025-02-25 12:36:51 +0530

    commit

--- app/src/main/java/com/mountreachsolution/animalbuddy/urls.java
+++ app/src/main/java/com/mountreachsolution/animalbuddy/urls.java
@@ -4,4 +4,6 @@ public class urls {
     public static String address="http://192.168.1.2:80/animalbuddy/";
     public static String userregister=address+"userregister.php";
     public static String login=address+"login.php";
+    public static String Profildata=address+"Profildata.php";
+    public static String profilimg=address+"profilimg.php";
 }
